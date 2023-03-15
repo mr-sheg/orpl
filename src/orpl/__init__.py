642072397089eb2ed2e3996cba911fb095bb1723
mr-sheg <guillaume.sheehy@polymtl.ca>
2023-03-15 11:48:12 -0400

    ! changes to run orpl as module to launch GUI

--- src/orpl/__init__.py
+++ src/orpl/__init__.py
@@ -4,9 +4,4 @@ ORPL
 the Open Raman Processing Library is a collection of tools for the processing, analysis and
 visualization of Raman spectrum.
 """
-ORPL_VERSION = 0.2
-
-if __name__ == "__main__":
-    from orpl.gui.orplGUI import launch_gui
-
-    launch_gui()
+ORPL_VERSION = 1.0
---
+++ src/orpl/__main__.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    from orpl.gui.orplGUI import launch_gui
+
+    launch_gui()
