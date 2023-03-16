6b409e277480f2c4562a7340117527967e420189
mr-sheg <guillaume.sheehy@polymtl.ca>
2023-03-16 14:03:21 -0400

    + added most orpl module import to init

--- src/orpl/__init__.py
+++ src/orpl/__init__.py
@@ -4,4 +4,17 @@ ORPL
 the Open Raman Processing Library is a collection of tools for the processing, analysis and
 visualization of Raman spectrum.
 """
+
+from orpl import (
+    baseline_removal,
+    calibration,
+    cosmic_ray,
+    datatypes,
+    file_io,
+    metrics,
+    normalization,
+    plot,
+    synthetic,
+)
+
 ORPL_VERSION = "1.0.2"
