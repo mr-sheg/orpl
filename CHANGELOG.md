a6b040850c33fb8b334b6a224509ab347cc5b684
mr-sheg <guillaume.sheehy@polymtl.ca>
2023-06-06 12:12:40 -0400

    ! Uniformized load spectra function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - Naming convention : replaced word "Select" with "Load"
+- Uniformized spectra load : now "accumulations" are always 2d Array even for a single accumulation.
 
 ### Fixed
 
--- src/orpl/datatypes.py
+++ src/orpl/datatypes.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Type
 
-from numpy import ndarray
+from numpy import expand_dims, ndarray
 
 
 @dataclass(frozen=True)
@@ -33,11 +33,11 @@ class Spectrum:
     mean_spectrum: ndarray = field(init=False)
 
     def __post_init__(self):
-        if self.accumulations.ndim > 1:
-            object.__setattr__(self, "naccumulations", self.accumulations.shape[1])
-            object.__setattr__(self, "mean_spectrum", self.accumulations.mean(axis=1))
-        else:
-            object.__setattr__(self, "naccumulations", 1)
-            object.__setattr__(self, "mean_spectrum", self.accumulations)
+        if self.accumulations.ndim < 2:
+            object.__setattr__(
+                self, "accumulations", expand_dims(self.accumulations, axis=1)
+            )
 
+        object.__setattr__(self, "naccumulations", self.accumulations.shape[1])
+        object.__setattr__(self, "mean_spectrum", self.accumulations.mean(axis=1))
         object.__setattr__(self, "nbins", self.accumulations.shape[0])
